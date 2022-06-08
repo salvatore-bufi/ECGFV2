2e54ff6759e6ea33d586ec30ec334b5057dc35ae
albertomancino <alberto.mancino94@gmail.com>
2022-06-08 15:23:41 +0200

    added datasets and split

--- start_experimentsOLD.py
+++ start_experimentsOLD.py
@@ -3,7 +3,7 @@ import argparse
 
 parser = argparse.ArgumentParser(description="Run training and evaluation.")
 parser.add_argument('--model', type=str, default='egcfv2')
-parser.add_argument('--dataset', type=str, default='salv_prova_yahoo')
+parser.add_argument('--dataset', type=str, default='yahoo_10core')
 args = parser.parse_args()
 
 run_experiment(f"config_files/{args.model}/{args.dataset}.yml")

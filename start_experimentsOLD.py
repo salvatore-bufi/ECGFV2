fa97129ec141fdedaf19aa27dffde5837aeb9665
salvatore-bufi <bufi.salvatore.94@gmail.com>
2022-06-08 18:23:29 +0200

    updated start_exp

--- start_experimentsOLD.py
+++ start_experimentsOLD.py
@@ -3,7 +3,7 @@ import argparse
 
 parser = argparse.ArgumentParser(description="Run training and evaluation.")
 parser.add_argument('--model', type=str, default='egcfv2')
-parser.add_argument('--dataset', type=str, default='yahoo_prova')
+parser.add_argument('--dataset', type=str, default='yahoo_exploration')
 args = parser.parse_args()
 
 run_experiment(f"config_files/{args.model}/{args.dataset}.yml")
